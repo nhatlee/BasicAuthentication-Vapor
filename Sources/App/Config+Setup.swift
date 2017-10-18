8cf676cb63c9d03e9e71b901d73a0c4e4582b658
nhatlee <lenhathk@gmail.com>
2017-10-18 15:06:44 +0700

    basic auth

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -1,4 +1,5 @@
 import LeafProvider
+import FluentProvider
 
 extension Config {
     public func setup() throws {
@@ -7,10 +8,18 @@ extension Config {
         Node.fuzzy = [JSON.self, Node.self]
 
         try setupProviders()
+        try setupPreparations()
     }
 
     /// Configure providers
     private func setupProviders() throws {
         try addProvider(LeafProvider.Provider.self)
+        try addProvider(FluentProvider.Provider.self)
+    }
+    
+    /// Add all models that should have their
+    /// schemas prepared before the app boots
+    private func setupPreparations() throws {
+        preparations.append(User.self)
     }
 }
